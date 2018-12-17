a3b5e5c7adeb18e2dd5bcddc667c96861937b894
Jeremy Lee <jlee00@stuy.edu>
2018-12-17 09:59:13 -0500

    added Node

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,36 @@
+public class Node{
+  private int data;
+  private Node prev, next;
+
+  public Node(int data, Node prev, Node next){
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+
+  public int getData(){
+    return data;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev(){
+    return prev;
+  }
+  public void setData(int data){
+    this.data = data;
+  }
+  public void setNext(Node next){
+    this.next = next;
+  }
+  public void setPrev(Node prev){
+    this.prev = prev;
+  }
+}
+
 public class MyLinkedList {
   private Node start, end;
-  private size;
+  private int size;
 
   public MyLinkedList(){
 
@@ -9,4 +39,6 @@ public class MyLinkedList {
   public int size(){
     return size;
   }
+
+
 }
